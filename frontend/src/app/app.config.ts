a0bda08ed87679ffce1dcae61414bff449c91ecd
tornixtech <francis.amani@tornixtetch.com>
2024-07-15 17:45:36 +0000

    login

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LoginComponent } from './auth/login/login.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  
 };
