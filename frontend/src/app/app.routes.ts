55b0f6f8b5231b5b7748835ca99238986e92fa8c
tornixtech <francis.amani@tornixtetch.com>
2024-07-22 15:22:16 +0000

    ok

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthComponent } from '../app/auth/auth.component';
 import { UserComponent } from './user/user.component';
+import { SellingPageComponent } from './user/selling-page/selling-page.component';
 
 // import { ProductManagementComponent } from './product-management/product-management.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'selling-page', component: UserComponent },
+    { path: 'selling-page', component: SellingPageComponent },
 
     // {
     //     path: '',
