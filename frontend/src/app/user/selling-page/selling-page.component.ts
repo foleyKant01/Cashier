b469470b52748919f2f5b630cae08a2a2bc4b184
foleyKant <krayediego@gmail.com>
2024-07-22 13:36:44 +0200

    création du composant selling page

---
+++ frontend/src/app/user/selling-page/selling-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selling-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './selling-page.component.html',
+  styleUrl: './selling-page.component.scss'
+})
+export class SellingPageComponent {
+
+}
