117c424e2042454b904ba8a95faf6cacd058fa2b
tornixtech <francis.amani@tornixtetch.com>
2024-07-24 17:03:55 +0000

    ok

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,22 +8,22 @@ import { SellingPageComponent } from './user/selling-page/selling-page.component
 // import { ProductManagementComponent } from './product-management/product-management.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'selling-page', component: SellingPageComponent },
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'selling-page', component: SellingPageComponent },
 
-    // {
-    //     path: '',
-    //     redirectTo: 'auth',
-    //     pathMatch: 'full'
-    //   },
-    //   {
-    //     path: 'auth',
-    //     loadChildren: () => import('../app/auth/auth.component').then( m => m.AuthComponent)
-    //   },
-    //   {
-    //     path: 'user',
-    //     loadChildren: () => import('../app/user/user.component').then( m => m.UserComponent)
-    //   },
+    {
+        path: '',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+      },
+      {
+        path: 'auth',
+        loadChildren: () => import('../app/auth/auth.component').then( m => m.AuthComponent)
+      },
+      {
+        path: 'user',
+        loadChildren: () => import('../app/user/user.component').then( m => m.UserComponent)
+      },
 
 ];
