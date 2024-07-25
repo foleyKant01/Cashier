1debd841dfe08805fd58ed00a7f7570599f3e200
tornixtech <francis.amani@tornixtetch.com>
2024-07-25 15:08:04 +0000

    route modified

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { routes } from '../app.routes';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forChild(routes)
+
   ]
 })
 export class AdminModule { }
